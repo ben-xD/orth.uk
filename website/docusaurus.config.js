509a29c261e6985d031cb4d63ad5622222efa692
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-11-04 12:33:22 +0000

    format(website): format docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,28 +1,26 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const path = require('path')
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const path = require("path");
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Ben Butterworth',
-  tagline: 'Your life is purchased by where you spend your attention.',
-  url: 'https://orth.uk',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Ben-xD', // Usually your GitHub org/user name.
-  projectName: 'orth.uk', // Usually your repo name.
-  plugins: [
-    path.resolve(__dirname, 'plugins/service-worker-remover-plugin'),
-  ],
+  title: "Ben Butterworth",
+  tagline: "Your life is purchased by where you spend your attention.",
+  url: "https://orth.uk",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Ben-xD", // Usually your GitHub org/user name.
+  projectName: "orth.uk", // Usually your repo name.
+  plugins: [path.resolve(__dirname, "plugins/service-worker-remover-plugin")],
   presets: [
     [
       // docs: https://docusaurus.io/docs/using-plugins#docusauruspreset-classic
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: false,
         // docs: {
@@ -30,131 +28,123 @@ const config = {
         //   editUrl: 'https://github.com/ben-xD/orth.uk',
         // },
         // docs: https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-pages#mdxPageComponent
-        pages: {
-          mdxPageComponent: path.resolve(__dirname, 'src/components/mdxPageComponent.tsx')
-        },
         blog: {
-          path: './blog',
-          routeBasePath: '/',
-          blogSidebarTitle: 'All posts',
-          blogSidebarCount: 'ALL',
+          path: "./blog",
+          routeBasePath: "/",
+          blogSidebarTitle: "All posts",
+          blogSidebarCount: "ALL",
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/ben-xD/orth.uk/blob/main/website',
-          // defaults to '@theme/BlogPostPage'
-          blogPostComponent: path.resolve(__dirname, 'src/components/BlogPostPage.tsx')
+          editUrl: "https://github.com/ben-xD/orth.uk/blob/main/website",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
 
-  themeConfig:
-    ({
-      navbar: {
-        title: 'Ben Butterworth',
-        logo: {
-          alt: 'Site Logo',
-          src: 'img/logo.svg',
+  themeConfig: {
+    navbar: {
+      title: "Ben Butterworth",
+      logo: {
+        alt: "Site Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        // {to: '/blog', label: 'Blog', position: 'left'},
+        // {
+        //   type: 'doc',
+        //   docId: 'intro',
+        //   position: 'left',
+        //   label: 'Guides',
+        // },
+        { to: "/about", label: "About", position: "left" },
+        { to: "/projects", label: "Projects", position: "left" },
+        { to: "/feedback", label: "Feedback", position: "left" },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Navigation",
+          items: [
+            {
+              label: "Home",
+              to: "/",
+            },
+            {
+              label: "About",
+              to: "/about",
+            },
+            {
+              label: "Projects",
+              to: "/projects",
+            },
+            {
+              label: "All Blog Posts",
+              to: "/archive",
+            },
+            {
+              label: "Video Room",
+              to: "https://orth.uk/video",
+            },
+            {
+              label: "Feedback",
+              to: "/feedback",
+            },
+          ],
         },
-        items: [
-          // {to: '/blog', label: 'Blog', position: 'left'},
-          // {
-          //   type: 'doc',
-          //   docId: 'intro',
-          //   position: 'left',
-          //   label: 'Guides',
-          // },
-          { to: '/about', label: 'About', position: 'left' },
-          { to: '/projects', label: 'Projects', position: 'left' },
-          { to: '/feedback', label: 'Feedback', position: 'left' },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Navigation',
-            items: [
-              {
-                label: 'Home',
-                to: '/',
-              },
-              {
-                label: 'About',
-                to: '/about',
-              },
-              {
-                label: 'Projects',
-                to: '/projects',
-              },
-              {
-                label: 'All Blog Posts',
-                to: '/archive',
-              },
-              {
-                label: 'Video Room',
-                to: 'https://orth.uk/video',
-              },
-              {
-                label: 'Feedback',
-                to: '/feedback',
-              },
-            ],
-          },
-          {
-            title: 'Profiles',
-            items: [
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/orth_uk',
-              },
-              {
-                label: 'LinkedIn',
-                href: 'https://www.linkedin.com/in/bbutterworth/',
-              },
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/users/7365866/ben-butterworth',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/ben-xD',
-              },
-            ],
-          },
-          {
-            title: 'Favourite websites, projects and tools',
-            items: [
-              {
-                label: 'High Scalability',
-                href: 'http://highscalability.com/',
-              },
-              {
-                label: 'Hacker News',
-                href: 'http://news.ycombinator.com/',
-              },
-              {
-                label: 'Flutter',
-                href: 'https://flutter.dev/'
-              },
-              {
-                label: 'Figma',
-                href: 'http://figma.com'
-              }
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Ben Butterworth`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        {
+          title: "Profiles",
+          items: [
+            {
+              label: "Twitter",
+              href: "https://twitter.com/orth_uk",
+            },
+            {
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/in/bbutterworth/",
+            },
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/users/7365866/ben-butterworth",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/ben-xD",
+            },
+          ],
+        },
+        {
+          title: "Favourite websites, projects and tools",
+          items: [
+            {
+              label: "High Scalability",
+              href: "http://highscalability.com/",
+            },
+            {
+              label: "Hacker News",
+              href: "http://news.ycombinator.com/",
+            },
+            {
+              label: "Flutter",
+              href: "https://flutter.dev/",
+            },
+            {
+              label: "Figma",
+              href: "http://figma.com",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Ben Butterworth`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
 };
 
 module.exports = config;
--- website/src/theme/README.md
+++ website/src/theme/README.md
@@ -3,3 +3,15 @@
 Originally, I tried using docusaurus' swizzle functionality. For example, by running `pnpm run swizzle` and selecting `MDXPage` to be swizzled. See https://docusaurus.io/docs/2.x/swizzling for more information.
 
 However, it generates `.js` and `.d.ts` instead of `.tsx` files. Instead, I just clone docusaurus, check out the git tag corresponding to the version I'm using, and then copy the typescript file.
+
+Files here will be used by docusaurus automatically. I previously did:
+```
+// This allows me to add comments to any page if `isCommentsEnabled: true` is in the header.
+mdxPageComponent: path.resolve(__dirname, 'src/components/mdxPageComponent.tsx')
+```
+
+and
+
+```js
+blogPostComponent: path.resolve(__dirname, 'src/components/BlogPostPage.tsx')
+```
